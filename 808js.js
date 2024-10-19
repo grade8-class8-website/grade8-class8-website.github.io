d10039ef945661f929e9fc3e52421155d1318bbd
grade8-class8-website <2362666741@qq.com>
2024-10-19 16:26:16 +0800

    Update 808js.js

--- 808js.js
+++ 808js.js
@@ -6,3 +6,10 @@ function sidebaropen(){
 function sidebarclose(){
   document.getElementById("close-menu").style.display = "none";
 }
+
+function get_set() {
+  var r = document.querySelector(':root');
+  var bodyH = window.innerHeight + "px";
+  r.style.setProperty('--side-bar-height', bodyH);
+}
+
