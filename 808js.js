caf362c46865afdbe4019745ab3b06e63433325f
grade8-class8-website <2362666741@qq.com>
2024-10-19 11:54:10 +0800

    Create 808js.js

---
+++ 808js.js
@@ -0,0 +1,8 @@
+function sidebaropen(){
+  document.getElementById("close-menu").style.display = "block";
+}
+
+
+function sidebarclose(){
+  document.getElementById("close-menu").style.display = "none";
+}
