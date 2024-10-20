8dfcc3f45d1bc71121a2c0c2eb2aede95e595f76
grade8-class8-website <2362666741@qq.com>
2024-10-20 14:27:47 +0800

    Update 808js.js

--- 808js.js
+++ 808js.js
@@ -1,28 +1,41 @@
 const sidebarcomputer = document.getElementsByClassName("computer-open-bar");
-const rootElement = document.querySelector(':root');
 
 function sidebaropen() {
-  document.getElementById("close-menu").style.display = "block";
+    document.getElementById("close-menu").style.display = "block";
 }
 
 function sidebarclose() {
-  document.getElementById("close-menu").style.display = "none";
-  rootElement.style.setProperty('--into', 'leave');
+    document.getElementById("close-menu").style.display = "none";
+    const rootElement = document.querySelector(':root');
+    rootElement.style.setProperty('--into', 'leave');
 }
 
-sidebarcomputer.forEach(element => {
-  element.addEventListener('mouseleave', function(event) {
-    rootElement.style.setProperty('--into', 'leave');
-  });
+function pseudoForEach(collection, callback) {
+    for (let i = 0; i < collection.length; i++) {
+        callback(collection[i], i, collection);
+    }
+}
+
+pseudoForEach(sidebarcomputer, element => {
+    element.addEventListener('mouseleave', function(event) {
+        const rootElement = document.querySelector(':root');
+        rootElement.style.setProperty('--into', 'leave');
+    });
 });
 
+
 function get_set() {
-  const bodyheight = Math.max(
-    document.body.scrollHeight, document.documentElement.scrollHeight,
-    document.body.offsetHeight, document.documentElement.offsetHeight,
-    document.body.clientHeight, document.documentElement.clientHeight
-  );
-  rootElement.style.setProperty('--side-bar-height', bodyheight + 'px');
+    const tempDiv = document.createElement('div');
+    tempDiv.style.position = 'absolute';
+    tempDiv.style.visibility = 'hidden';
+    tempDiv.style.width = '1px';
+    tempDiv.style.height = 'auto';
+    document.body.appendChild(tempDiv);
+    const scrollHeight = tempDiv.scrollHeight + 'px';
+    document.body.removeChild(tempDiv);
+    const rootElement = document.querySelector(':root');
+    rootElement.style.setProperty('--side-bar-height', scrollHeight);
+    console.log(scrollHeight)
 }
 
-get_set();
+get_set()
