37283781edfefad61986a001c01a3087167ca46a
grade8-class8-website <2362666741@qq.com>
2024-10-20 16:35:57 +0800

    Update 808js.js

--- 808js.js
+++ 808js.js
@@ -1,36 +1,12 @@
-const sidebarcomputer = document.getElementsByClassName("computer-open-bar");
 
 function sidebaropen() {
     document.getElementById("close-menu").style.display = "block";
-    const rootElement = document.querySelector(':root');
-    rootElement.style.setProperty('--into', 'into');
 }
-
 function sidebarclose() {
     document.getElementById("close-menu").style.display = "none";
-    const rootElement = document.querySelector(':root');
-    rootElement.style.setProperty('--into', 'leave');
 }
 
-function pseudoForEach(collection, callback) {
-    for (let i = 0; i < collection.length; i++) {
-        callback(collection[i], i, collection);
-    }
-}
 
-pseudoForEach(sidebarcomputer, element => {
-    element.addEventListener('mouseleave', function(event) {
-        const rootElement = document.querySelector(':root');
-        rootElement.style.setProperty('--into', 'leave');
-    });
-});
-
-pseudoForEach(sidebarcomputer, element => {
-    element.addEventListener('mouseenter', function(event) {
-        const rootElement = document.querySelector(':root');
-        rootElement.style.setProperty('--into', 'into');
-    });
-});
 
 
 function get_set() {
